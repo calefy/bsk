13eab073388e47b1d69a6bf70d0c1a4fb8644691
陈林飞 <chenlinfei@ezijing.com>
2016-02-12 20:40:35 +0800

    添加本地时区设置

--- common/config/base.php
+++ common/config/base.php
@@ -5,6 +5,7 @@ $config = [
     'extensions' => require(__DIR__ . '/../../vendor/yiisoft/extensions.php'),
     'sourceLanguage'=>'en-US',
     'language'=>'en-US',
+    'timeZone' => 'Asia/Shanghai',
     'bootstrap' => ['log'],
     'components' => [
 
