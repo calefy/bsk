c879fa28f7b1a762d5d7746dc4e3969acc44bd01
陈林飞 <chenlinfei@ezijing.com>
2016-03-23 10:09:13 +0800

    修改分类view展示

--- backend/views/bsk-category/view.php
+++ backend/views/bsk-category/view.php
@@ -19,45 +19,60 @@ $types = BskCategoryOther::types();
 ?>
 <div class="bsk-category-other-view">
 
-    <p>
-        <?php echo Html::a(Yii::t('backend', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?php echo Html::a(Yii::t('backend', 'Delete'), ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => Yii::t('backend', 'Are you sure you want to delete this item?'),
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
 
-    <?php echo DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            [
-                'attribute' => 'type',
-                'value' => $types[$model->type],
-            ],
-            [
-                'attribute' => 'grade_id',
-                'value' => $categories[$model->grade_id],
-            ],
-            [
-                'attribute' => 'semester_id',
-                'value' => isset($categories[$model->semester_id]) ? $categories[$model->semester_id] : $model->semester_id,
-            ],
-            [
-                'attribute' => 'science_id',
-                'value' => $categories[$model->science_id],
-            ],
-            [
-                'attribute' => 'syllabus_id',
-                'value' => isset($categories[$model->syllabus_id]) ? $categories[$model->syllabus_id] : $model->syllabus_id,
-            ],
-            'updated_at:datetime',
-            'created_at:datetime',
-        ],
-    ]) ?>
+    <?php // echo DetailView::widget([
+        //'model' => $model,
+        //'attributes' => [
+        //    'id',
+        //    [
+        //        'attribute' => 'type',
+        //        'value' => $types[$model->type],
+        //    ],
+        //    [
+        //        'attribute' => 'grade_id',
+        //        'value' => $categories[$model->grade_id],
+        //    ],
+        //    [
+        //        'attribute' => 'semester_id',
+        //        'value' => isset($categories[$model->semester_id]) ? $categories[$model->semester_id] : $model->semester_id,
+        //    ],
+        //    [
+        //        'attribute' => 'science_id',
+        //        'value' => $categories[$model->science_id],
+        //    ],
+        //    [
+        //        'attribute' => 'syllabus_id',
+        //        'value' => isset($categories[$model->syllabus_id]) ? $categories[$model->syllabus_id] : $model->syllabus_id,
+        //    ],
+        //    'updated_at:datetime',
+        //    'created_at:datetime',
+        //],
+    //]) ?>
+
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <p class="pull-right">
+                <?php echo Html::a(Yii::t('backend', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+                <?php echo Html::a(Yii::t('backend', 'Delete'), ['delete', 'id' => $model->id], [
+                    'class' => 'btn btn-danger',
+                    'data' => [
+                        'confirm' => Yii::t('backend', 'Are you sure you want to delete this item?'),
+                        'method' => 'post',
+                    ],
+                ]) ?>
+            </p>
+            <div>
+                <p><strong><?=$types[$model->type]?>分类</strong></p>
+                <p>
+                    <?=$categories[$model->grade_id] ?>
+                    &gt;
+                    <?=isset($categories[$model->semester_id]) ? $categories[$model->semester_id] . ' &gt;' : ''?>
+                    <?=$categories[$model->science_id]?>
+                    <?=isset($categories[$model->syllabus_id]) ? ' &gt; '.$categories[$model->syllabus_id] : ''?>
+                </p>
+            </div>
+        </div>
+    </div>
 
     <?php
         echo TreeView::widget([
@@ -65,10 +80,10 @@ $types = BskCategoryOther::types();
             // use the Product model you have in the previous step
             'query' => BskCategory::find()->andWhere(['root' => $model->category_id])->addOrderBy('root, lft'),
             'headingOptions'=>['label'=> '分类设置'],
-            //'isAdmin' => true,
             'iconEditSettings' => [ // 不显示icon的编辑部分
                 'show' => 'none',
             ],
+            'displayValue' => $model->category_id,
             'allowNewRoots' => false,
         ]);
     ?>
