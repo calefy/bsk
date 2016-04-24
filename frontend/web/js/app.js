7ea33b4b837741ae267425099d3e2a4f3bb855da
陈林飞 <chenlinfei@ezijing.com>
2016-04-24 22:46:15 +0800

    front端禁止mathjax菜单显示

--- frontend/web/js/app.js
+++ frontend/web/js/app.js
@@ -1,3 +1,12 @@
 /**
  *  @author Eugene Terentev <eugene@terentev.net>
  */
+// 配置mathjax不显示右键菜单，下面两项联合配置生效
+setTimeout(function() {
+    if (window.MathJax) {
+        MathJax.Hub.Config({
+            context: 'Browser',
+            showMathMenu: false
+        });
+    }
+}, 200);
