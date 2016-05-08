b05ee91e7d1830eacb64913e3a8304fe9bd78dfc
陈林飞 <chenlinfei@ezijing.com>
2016-05-08 21:15:53 +0800

    去掉注册中的fb/google

--- frontend/modules/user/views/sign-in/signup.php
+++ frontend/modules/user/views/sign-in/signup.php
@@ -21,12 +21,14 @@ $this->params['breadcrumbs'][] = $this->title;
                 <div class="form-group">
                     <?php echo Html::submitButton(Yii::t('frontend', 'Signup'), ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
                 </div>
+                <?php /* ?>
                 <h2><?php echo Yii::t('frontend', 'Sign up with')  ?>:</h2>
                 <div class="form-group">
                     <?php echo yii\authclient\widgets\AuthChoice::widget([
                         'baseAuthUrl' => ['/user/sign-in/oauth']
                     ]) ?>
                 </div>
+                <?php */ ?>
             <?php ActiveForm::end(); ?>
         </div>
     </div>
